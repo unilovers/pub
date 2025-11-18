4ec11904e9d15506ead64a8f719091e4eaab4187
Ana Villas <anacarolina.vbp@gmail.com>
2025-11-18 20:48:59 -0300

    ✨ feat: Estrutura inicial de pastas do projeto

---
+++ pub/src/test/java/com/grupo_5/pub/PubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_5.pub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
