b41cfb2976e9ab55988f72d00414ed3e223fe917
Ana Villas <168604507+Anavillas@users.noreply.github.com>
2025-11-24 19:19:56 -0300

    Revert "Comanda renas"

--- pub/src/main/java/com/grupo_5/pub/Repository/ComandaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.grupo_5.pub.Repository;
-
-import com.grupo_5.pub.Model.Comanda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComandaRepository extends JpaRepository<Comanda, Long> {
-
-}
--- pub/src/main/java/com/grupo_5/pub/Repository/ItemComandaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupo_5.pub.Repository;
-
-import com.grupo_5.pub.Model.ItemComanda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemComandaRepository extends JpaRepository<ItemComanda, Long> {
-}
