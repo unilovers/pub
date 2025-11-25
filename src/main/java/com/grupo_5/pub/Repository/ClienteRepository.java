651bbb5178d59f6ab2eaf9a916a06b298ee7bec6
Ana Villas <anacarolina.vbp@gmail.com>
2025-11-25 15:05:59 -0300

    🔥 chore: Removidoss arquivos que davam conflito.

--- src/main/java/com/grupo_5/pub/Repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.grupo_5.pub.Repository;
-
-import com.grupo_5.pub.Model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
